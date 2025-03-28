ccf2af8ae306601cbf203f3d69eec1bd4370126a
manoj2003-code <manojnagpure2782003@gmail.com>
2025-03-29 00:11:35 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,8 @@
+function changeText() {
+  document.getElementById("btn").innerHTML ="Jai Shivaji Jai Bhavani "
+}
+
+function changeColor() {
+  document.getElementById("color").style.color="red";
+}
+
